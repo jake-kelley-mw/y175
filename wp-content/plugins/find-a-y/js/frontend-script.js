eade6667e387d4e744a36a6749e738bac953fec7
Jake Kelley <jake.kelley@mckenziewagner.com>
2025-11-06 23:12:11 +0000

    commit

--- wp-content/plugins/find-a-y/js/frontend-script.js
+++ wp-content/plugins/find-a-y/js/frontend-script.js
@@ -5,48 +5,32 @@
 
 jQuery(document).ready(function($) {
     
-    var searchCache = {};
-    
     $('#ymca-location-search').on('submit', function(e) {
         e.preventDefault();
         
         var zipCode = $('#ymca-zip-input').val().trim();
         
-        if (!zipCode) {
-            showError('Please enter a ZIP code');
+        // Validate ZIP code
+        if (!zipCode || zipCode.length < 5) {
+            $('.search-error').text('Please enter a valid ZIP code').show();
             return;
         }
         
-        if (!isValidZip(zipCode)) {
-            showError('Please enter a valid 5-digit ZIP code');
-            return;
-        }
+        // Track ZIP code search
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'zip_code_search',
+            'zip_code': zipCode,
+            'page_location': window.location.pathname
+        });
         
-        hideError();
-        
-        if (searchCache[zipCode]) {
-            displayResults(searchCache[zipCode], zipCode);
-            return;
-        }
-        
-        performSearch(zipCode);
-    });
-    
-    $('#ymca-zip-input').on('input', function() {
-        var value = $(this).val();
-        value = value.replace(/[^0-9-]/g, '');
-        $(this).val(value);
-    });
-    
-    function isValidZip(zip) {
-        return /^\d{5}(-\d{4})?$/.test(zip);
-    }
-    
-    function performSearch(zipCode) {
-        $('.find-a-y-search-form').slideUp();
-        $('.find-a-y-results').hide();
+        // Show loading state
         $('.find-a-y-loading').show();
+        $('.find-a-y-search-form').hide();
+        $('.find-a-y-results').hide();
+        $('.search-error').hide();
         
+        // Make AJAX request
         $.ajax({
             url: findAY.ajax_url,
             type: 'POST',
@@ -57,102 +41,87 @@ jQuery(document).ready(function($) {
             },
             success: function(response) {
                 $('.find-a-y-loading').hide();
-                $('.find-a-y-search-form').slideDown();
                 
                 if (response.success) {
-                    searchCache[zipCode] = response.data;
+                    var resultsCount = response.data.length;
+                    
+                    // Track successful search with results count
+                    window.dataLayer.push({
+                        'event': 'zip_search_results',
+                        'zip_code': zipCode,
+                        'results_count': resultsCount
+                    });
+                    
                     displayResults(response.data, zipCode);
                 } else {
-                    showError(response.data || 'No locations found near that ZIP code. Please try a different ZIP code.');
+                    // Track no results found
+                    window.dataLayer.push({
+                        'event': 'zip_search_no_results',
+                        'zip_code': zipCode
+                    });
+                    
+                    $('.search-error').text(response.data).show();
+                    $('.find-a-y-search-form').show();
                 }
             },
             error: function() {
                 $('.find-a-y-loading').hide();
-                $('.find-a-y-search-form').slideDown();
-                showError('An error occurred. Please try again.');
+                $('.search-error').text('An error occurred. Please try again.').show();
+                $('.find-a-y-search-form').show();
             }
         });
-    }
+    });
     
+    // Function to display results
     function displayResults(locations, zipCode) {
-        if (!locations || locations.length === 0) {
-            showError('No YMCA locations found near ZIP code ' + zipCode);
-            return;
-        }
+        var resultsHTML = '';
         
-        var resultsHtml = '';
-        
-        locations.forEach(function(location) {
-            resultsHtml += buildLocationCard(location);
+        $.each(locations, function(index, location) {
+            resultsHTML += '<div class="ymca-location-card">';
+            resultsHTML += '<div class="location-header">';
+            resultsHTML += '<h3 class="location-name">' + location.y_name + '</h3>';
+            if (location.distance) {
+                resultsHTML += '<span class="location-distance">' + location.distance + ' miles</span>';
+            }
+            resultsHTML += '</div>';
+            
+            resultsHTML += '<div class="location-address">';
+            resultsHTML += '<strong>Address:</strong><br>';
+            resultsHTML += location.address + '<br>';
+            if (location.address2) {
+                resultsHTML += location.address2 + '<br>';
+            }
+            resultsHTML += location.city + ', ' + location.state + ' ' + location.zip_code;
+            resultsHTML += '</div>';
+            
+            if (location.website) {
+                resultsHTML += '<div class="location-actions">';
+                resultsHTML += '<a href="' + location.website + '" class="location-link primary" target="_blank" ';
+                resultsHTML += 'data-location-name="' + location.y_name + '" ';
+                resultsHTML += 'data-location-zip="' + location.zip_code + '">';
+                resultsHTML += 'Visit Website</a>';
+                resultsHTML += '</div>';
+            }
+            
+            resultsHTML += '</div>';
         });
         
-        $('#ymca-results-list').html(resultsHtml);
+        $('#ymca-results-list').html(resultsHTML);
+        $('.results-heading').text('YMCA Locations near ' + zipCode);
+        $('.find-a-y-results').show();
         
-        var heading = locations.length + ' YMCA Location' + (locations.length !== 1 ? 's' : '') + ' Near You';
-        $('.results-heading').text(heading);
-        
-        $('.find-a-y-results').slideDown();
-        
-        $('html, body').animate({
-            scrollTop: $('.find-a-y-results').offset().top - 20
-        }, 500);
-    }
-    
-    function buildLocationCard(location) {
-        var card = '<div class="ymca-location-card">';
-        
-        card += '<div class="location-header">';
-        card += '<h3 class="location-name">' + escapeHtml(location.y_name) + '</h3>';
-        
-        if (location.distance !== null && location.distance !== undefined) {
-            card += '<span class="location-distance">' + location.distance + ' miles</span>';
-        }
-        
-        card += '</div>';
-        
-        card += '<div class="location-address">';
-        card += '<strong>Address:</strong><br>';
-        card += escapeHtml(location.formatted_address);
-        card += '</div>';
-        
-        card += '<div class="location-actions">';
-        
-        if (location.website) {
-            card += '<a href="' + escapeHtml(location.website) + '" target="_blank" class="location-link primary">';
-            card += 'Visit Website';
-            card += '</a> ';
-        }
-        
-        var mapsUrl = 'https://www.google.com/maps/search/?api=1&query=' + 
-                      encodeURIComponent(location.formatted_address);
-        card += '<a href="' + mapsUrl + '" target="_blank" class="location-link">';
-        card += 'Get Directions';
-        card += '</a>';
-        
-        card += '</div>';
-        
-        card += '</div>';
-        
-        return card;
-    }
-    
-    function showError(message) {
-        $('.search-error').text(message).slideDown();
-    }
-    
-    function hideError() {
-        $('.search-error').slideUp();
-    }
-    
-    function escapeHtml(text) {
-        if (!text) return '';
-        var map = {
-            '&': '&amp;',
-            '<': '&lt;',
-            '>': '&gt;',
-            '"': '&quot;',
-            "'": '&#039;'
-        };
-        return text.toString().replace(/[&<>"']/g, function(m) { return map[m]; });
+        // Track clicks on location websites
+        $('.location-link.primary').on('click', function() {
+            var locationName = $(this).data('location-name');
+            var locationZip = $(this).data('location-zip');
+            
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                'event': 'location_website_click',
+                'location_name': locationName,
+                'location_zip': locationZip,
+                'search_zip': zipCode
+            });
+        });
     }
 });
--- wp-content/themes/divi-child/functions.php
+++ wp-content/themes/divi-child/functions.php
@@ -151,3 +151,309 @@ function y175_timeline_assets() {
     );
 }
 add_action('wp_enqueue_scripts', 'y175_timeline_assets');
+
+
+
+
+
+
+// ---------- GOOGLE TRACKING IMPLEMENTATION ---------- 
+
+function ymca_175_add_gtm_head() {
+    ?>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K5JH3B4L');</script>
+    <!-- End Google Tag Manager -->
+    <?php
+}
+add_action('wp_head', 'ymca_175_add_gtm_head', 10);
+
+function ymca_175_add_gtm_body() {
+    ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K5JH3B4L"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php
+}
+add_action('wp_body_open', 'ymca_175_add_gtm_body', 10);
+
+/**
+ * Pushes events to dataLayer for GTM to process
+ */
+function ymca_175_event_tracking() {
+    ?>
+    <script>
+    (function() {
+        'use strict';
+        
+        // Helper function to push events to dataLayer
+        function trackEvent(eventName, eventData) {
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                'event': eventName,
+                ...eventData
+            });
+        }
+        
+        function initTracking() {
+            
+            // ============================================
+            // VIDEO TRACKING
+            // ============================================
+            
+            // Track launch video and embedded videos
+            var embeddedVideos = document.querySelectorAll('iframe[src*="gettyimages.com"], iframe[src*="youtube.com"], iframe[src*="vimeo.com"]');
+            embeddedVideos.forEach(function(iframe) {
+                var videoType = 'Unknown';
+                if (iframe.src.includes('gettyimages')) videoType = 'Getty Images';
+                if (iframe.src.includes('youtube')) videoType = 'YouTube';
+                if (iframe.src.includes('vimeo')) videoType = 'Vimeo';
+                
+                trackEvent('video_loaded', {
+                    'video_type': videoType,
+                    'video_title': iframe.title || 'Embedded Video',
+                    'page_location': window.location.pathname
+                });
+            });
+            
+            // Track native HTML5 videos
+            var videos = document.querySelectorAll('video');
+            videos.forEach(function(video) {
+                var hasPlayed = false;
+                var videoTitle = video.title || video.getAttribute('data-title') || 'Video';
+                
+                video.addEventListener('play', function() {
+                    if (!hasPlayed) {
+                        hasPlayed = true;
+                        trackEvent('video_play', {
+                            'video_title': videoTitle,
+                            'page_location': window.location.pathname
+                        });
+                    }
+                });
+                
+                video.addEventListener('ended', function() {
+                    trackEvent('video_complete', {
+                        'video_title': videoTitle,
+                        'page_location': window.location.pathname
+                    });
+                });
+                
+                // Track viewing milestones
+                var milestones = {25: false, 50: false, 75: false};
+                video.addEventListener('timeupdate', function() {
+                    var percentComplete = (video.currentTime / video.duration) * 100;
+                    
+                    if (percentComplete >= 25 && !milestones[25]) {
+                        milestones[25] = true;
+                        trackEvent('video_progress', {
+                            'video_title': videoTitle,
+                            'progress_percent': 25
+                        });
+                    }
+                    if (percentComplete >= 50 && !milestones[50]) {
+                        milestones[50] = true;
+                        trackEvent('video_progress', {
+                            'video_title': videoTitle,
+                            'progress_percent': 50
+                        });
+                    }
+                    if (percentComplete >= 75 && !milestones[75]) {
+                        milestones[75] = true;
+                        trackEvent('video_progress', {
+                            'video_title': videoTitle,
+                            'progress_percent': 75
+                        });
+                    }
+                });
+            });
+            
+            
+            // ============================================
+            // TIMELINE TRACKING
+            // ============================================
+            
+            // Track timeline items coming into view (scroll-based)
+            var timelineItems = document.querySelectorAll(
+                '.timeline-item, .et_pb_timeline_item, [class*="timeline-entry"], .timeline-card'
+            );
+            
+            if (timelineItems.length > 0 && 'IntersectionObserver' in window) {
+                var timelineObserver = new IntersectionObserver(function(entries) {
+                    entries.forEach(function(entry) {
+                        if (entry.isIntersecting) {
+                            var item = entry.target;
+                            var title = item.querySelector('h1, h2, h3, h4, .title, [class*="title"]');
+                            var titleText = title ? title.textContent.trim() : 'Timeline Item';
+                            var era = item.getAttribute('data-era') || item.getAttribute('data-year') || 'Unknown';
+                            
+                            trackEvent('timeline_item_viewed', {
+                                'timeline_item': titleText,
+                                'timeline_era': era
+                            });
+                            
+                            // Stop observing this item once tracked
+                            timelineObserver.unobserve(item);
+                        }
+                    });
+                }, {
+                    threshold: 0.5
+                });
+                
+                timelineItems.forEach(function(item) {
+                    timelineObserver.observe(item);
+                });
+            }
+            
+            // Track century anchor link clicks (#1800s, #1900s, #2000s)
+            var centuryLinks = document.querySelectorAll('a[href="#1800s"], a[href="#1900s"], a[href="#2000s"]');
+            centuryLinks.forEach(function(link) {
+                link.addEventListener('click', function() {
+                    var century = link.getAttribute('href').replace('#', '');
+                    
+                    trackEvent('timeline_century_navigation', {
+                        'century': century,
+                        'link_text': link.textContent.trim()
+                    });
+                });
+            });
+            
+            
+            // ============================================
+            // FORM SUBMISSION TRACKING (Contact Form 7)
+            // ============================================
+            
+            document.addEventListener('wpcf7mailsent', function(event) {
+                trackEvent('form_submission', {
+                    'form_type': 'Contact Form 7',
+                    'form_id': event.detail.contactFormId,
+                    'form_name': 'Story Submission'
+                });
+            });
+            
+            
+            // ============================================
+            // CTA BUTTON TRACKING
+            // ============================================
+            
+            // Track "Explore 175 Years" / Timeline CTA
+            var timelineCTA = document.querySelectorAll('a[href*="/175-years"]');
+            timelineCTA.forEach(function(link) {
+                link.addEventListener('click', function() {
+                    trackEvent('cta_click', {
+                        'cta_text': link.textContent.trim(),
+                        'cta_destination': link.href,
+                        'cta_type': 'Explore Timeline',
+                        'page_location': window.location.pathname
+                    });
+                });
+            });
+            
+            // Track "Share Your Story" CTA
+            var storyCTA = document.querySelectorAll('a[href*="/share"]');
+            storyCTA.forEach(function(link) {
+                link.addEventListener('click', function() {
+                    trackEvent('cta_click', {
+                        'cta_text': link.textContent.trim(),
+                        'cta_destination': link.href,
+                        'cta_type': 'Share Story',
+                        'page_location': window.location.pathname
+                    });
+                });
+            });
+            
+            // Track "In the News" CTA
+            var newsCTA = document.querySelectorAll('a[href*="/news"]');
+            newsCTA.forEach(function(link) {
+                link.addEventListener('click', function() {
+                    trackEvent('cta_click', {
+                        'cta_text': link.textContent.trim(),
+                        'cta_destination': link.href,
+                        'cta_type': 'News',
+                        'page_location': window.location.pathname
+                    });
+                });
+            });
+            
+            // Track link to main YMCA site (logo or footer links)
+            var ymcaLinks = document.querySelectorAll('a[href*="ymca.org"]');
+            ymcaLinks.forEach(function(link) {
+                link.addEventListener('click', function() {
+                    trackEvent('external_link_click', {
+                        'link_text': link.textContent.trim() || 'YMCA Logo',
+                        'link_url': link.href,
+                        'link_destination': 'YMCA.org'
+                    });
+                });
+            });
+            
+            
+            // ============================================
+            // SOCIAL MEDIA PROFILE LINK TRACKING
+            // ============================================
+            
+            var socialLinks = document.querySelectorAll(
+                'a[href*="facebook.com"], a[href*="instagram.com"], a[href*="twitter.com"], ' +
+                'a[href*="linkedin.com"], a[href*="youtube.com"], a[href*="tiktok.com"]'
+            );
+            
+            socialLinks.forEach(function(link) {
+                link.addEventListener('click', function() {
+                    var platform = 'Unknown';
+                    var href = link.href.toLowerCase();
+                    
+                    if (href.includes('facebook.com')) platform = 'Facebook';
+                    else if (href.includes('instagram.com')) platform = 'Instagram';
+                    else if (href.includes('twitter.com') || href.includes('x.com')) platform = 'Twitter/X';
+                    else if (href.includes('linkedin.com')) platform = 'LinkedIn';
+                    else if (href.includes('youtube.com')) platform = 'YouTube';
+                    else if (href.includes('tiktok.com')) platform = 'TikTok';
+                    
+                    trackEvent('social_profile_click', {
+                        'social_platform': platform,
+                        'profile_url': link.href,
+                        'page_location': window.location.pathname
+                    });
+                });
+            });
+            
+            
+            // ============================================
+            // PARTNERSHIP/SPONSOR TRACKING
+            // ============================================
+            
+            var sponsorLinks = document.querySelectorAll('[class*="partner"], [class*="sponsor"]');
+            sponsorLinks.forEach(function(link) {
+                if (link.tagName === 'A') {
+                    link.addEventListener('click', function() {
+                        var sponsorName = link.getAttribute('data-sponsor') || 
+                                        link.querySelector('img')?.alt || 
+                                        'Unknown Sponsor';
+                        
+                        trackEvent('sponsor_click', {
+                            'sponsor_name': sponsorName,
+                            'sponsor_url': link.href
+                        });
+                    });
+                }
+            });
+            
+        }
+        
+        // Initialize tracking when DOM is ready
+        if (document.readyState === 'loading') {
+            document.addEventListener('DOMContentLoaded', initTracking);
+        } else {
+            initTracking();
+        }
+        
+    })();
+    </script>
+    <?php
+}
+add_action('wp_footer', 'ymca_175_event_tracking', 20);
